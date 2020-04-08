acd10675463b82a783d95fe3892facdd0c01c447
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-04-08 14:41:47 +0300

    Add listeners to input data folders

--- scripts/process_cloudnet.py
+++ scripts/process_cloudnet.py
@@ -1,32 +1,84 @@
 #!venv/bin/python3
 """Master script for CloudnetPy processing."""
+import time
 import argparse
 import configparser
 import yaml
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import numpy as np
 
 
 def main():
     site_name = ARGS.site[0]
 
-    main_config = _read_config('main')
-    site_config = _read_config(site_name)
+    conf = {'main': _read_config('main'),
+            'site': _read_config(site_name)}
 
-    site_data = _read_site_info(main_config, site_name)
+    site_data = _read_site_info(conf, site_name)
+
+    paths_to_listen = _build_paths_to_listen(conf, site_name)
+    observers = [_add_watcher(path) for path in paths_to_listen]
+
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        for obs in observers:
+            obs.stop()
+    for obs in observers:
+        obs.join()
 
 
-def _read_config(name):
+def _read_config(conf_type):
     config = configparser.ConfigParser()
-    config.read(f"config/{name}.ini")
+    config.read(f"config/{conf_type}.ini")
     return config
 
 
-def _read_site_info(config, site_name):
-    site_file = f"{config['PATH']['resources']}Site.yml"
+def _read_site_info(conf, site_name):
+    site_file = f"{conf['main']['PATH']['resources']}Site.yml"
     with open(site_file, 'r') as stream:
         data = yaml.safe_load(stream)
-    for _, item in data['items'].items():
+    for item in data['items'].values():
         if item['id'] == site_name:
             return item
+
+
+def _build_paths_to_listen(conf, site_name):
+    def _get_root(key):
+        target = 'uncalibrated' if key == 'input' else 'calibrated'
+        return '/'.join((conf['main']['PATH'][key], site_name, target))
+
+    instruments = conf['site']['INSTRUMENTS']
+    lidar_path = '/'.join((_get_root('input'), instruments['lidar']))
+    radar_path = '/'.join((_get_root('input'), instruments['radar']))
+    model_path = '/'.join((_get_root('output'), instruments['model']))
+    return lidar_path, radar_path, model_path
+
+
+def _add_watcher(path):
+    observer = Observer()
+    observer.schedule(_Sniffer(path), path=path, recursive=True)
+    observer.start()
+    return observer
+
+
+class _Sniffer(FileSystemEventHandler):
+    def __init__(self, paths):
+        self._paths = paths
+        self._timestamp = time.time()
+
+    def on_modified(self, event):
+        """Actually seems to track both NEW and MODIFIED files."""
+        if event.is_directory:
+            return
+        # Hack to prevent watchdog bug that spawns multiple events.
+        # See: https://github.com/gorakhargosh/watchdog/issues/93
+        time.sleep(1)
+        if (time.time() - self._timestamp) > 1.5:
+            print(event.src_path)
+        self._timestamp = time.time()
 
 
 if __name__ == "__main__":
