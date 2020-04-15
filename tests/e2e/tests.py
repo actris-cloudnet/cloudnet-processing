fad2ad85b5c90052bb104e4c920382d4dda8a4db
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-04-15 11:36:53 +0300

    Modify e2e test method names

--- tests/e2e/tests.py
+++ tests/e2e/tests.py
@@ -13,30 +13,30 @@ class TestCloudnetProcessing:
         self.date = params['date'].replace('-', '')
         self.year = self.date[:4]
 
-    def test_that_lidar_files_are_concatenated(self):
+    def test_that_concatenates_lidar_files(self):
         file = '/'.join((self.input, self.site, 'uncalibrated/chm15k',
                          self.year, f"chm15k_{self.date}.nc"))
         assert os.path.isfile(file)
 
-    def test_that_calibrated_lidar_file_exists(self):
+    def test_that_creates_calibrated_lidar_file(self):
         file = '/'.join((self.output, self.site, 'calibrated/chm15k',
                          self.year, f"{self.date}_{self.site}_chm15k.nc"))
         assert os.path.isfile(file)
         assert netCDF4.Dataset(file).cloudnet_file_type == 'lidar'
 
-    def test_that_calibrated_radar_file_exists(self):
+    def test_that_creates_calibrated_radar_file(self):
         file = '/'.join((self.output, self.site, 'calibrated/rpg-fmcw-94',
                          self.year, f"{self.date}_{self.site}_rpg-fmcw-94.nc"))
         assert os.path.isfile(file)
         assert netCDF4.Dataset(file).cloudnet_file_type == 'radar'
 
-    def test_that_categorize_file_exists(self):
+    def test_that_creates_categorize_file(self):
         file = '/'.join((self.output, self.site, 'processed/categorize',
                          self.year, f"{self.date}_{self.site}_categorize.nc"))
         assert os.path.isfile(file)
         assert netCDF4.Dataset(file).cloudnet_file_type == 'categorize'
 
-    def test_that_product_files_exist(self):
+    def test_that_creates_product_files(self):
         products = ['iwc-Z-T-method', 'lwc-scaled-adiabatic', 'classification']
         file_types = ['iwc', 'lwc', 'classification']
         for product, file_type in zip(products, file_types):
