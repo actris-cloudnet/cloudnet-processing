0d3329fb6948601ab5007b3a70aa509dc598fe02
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-04-24 17:57:25 +0300

    Give data path as argument

--- scripts/plot-quicklooks.py
+++ scripts/plot-quicklooks.py
@@ -11,14 +11,13 @@ file_paths = importlib.import_module("operational-processing").file_paths
 
 
 def main():
-    root = _get_root_path()
-    for file in Path(root).rglob('*.nc'):
+    for file in Path(ARGS.data_path[0]).rglob('*.nc'):
         if not _is_date_in_range(file):
             continue
         nc_file_name = str(file)
         image_name = nc_file_name.replace('nc', 'png')
-        file_type = _get_file_type(nc_file_name)
         try:
+            file_type = _get_file_type(nc_file_name)
             fields, max_alt = _get_fields_for_plot(file_type)
         except NotImplementedError:
             continue
@@ -28,33 +27,24 @@ def main():
                             image_name=image_name, max_y=max_alt)
 
 
-def _get_root_path():
-    if ARGS.output:
-        root = ARGS.output
-    else:
-        config = process_utils.read_conf(ARGS)
-        root = config['main']['PATH']['output']
-    return '/'.join((root, ARGS.site[0]))
-
-
-def _is_plottable(image_name):
-    if os.path.isfile(image_name) and not ARGS.overwrite:
-        return False
-    return True
-
-
-def _get_file_type(nc_file_name):
-    nc = netCDF4.Dataset(nc_file_name)
-    file_type = nc.cloudnet_file_type
-    nc.close()
-    return file_type
-
-
 def _is_date_in_range(path):
-    date_in_file = int(path.name[:8])
+    date_in_file = path.name[:8]
+    if not date_in_file.isdigit():
+        return False
+    date_in_file = int(date_in_file)
     start = int(ARGS.start.replace('-', ''))
     stop = int(ARGS.stop.replace('-', ''))
     return start <= date_in_file < stop
+
+
+def _get_file_type(nc_file_name):
+    attr_name = 'cloudnet_file_type'
+    nc = netCDF4.Dataset(nc_file_name)
+    if not hasattr(nc, attr_name):
+        raise NotImplementedError
+    file_type = getattr(nc, attr_name)
+    nc.close()
+    return file_type
 
 
 def _get_fields_for_plot(cloudnet_file_type):
@@ -73,15 +63,19 @@ def _get_fields_for_plot(cloudnet_file_type):
     return fields, max_alt
 
 
+def _is_plottable(image_name):
+    if os.path.isfile(image_name) and not ARGS.overwrite:
+        return False
+    return True
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot quickloks from Cloudnet data.')
-    parser.add_argument('site', nargs='+', help='Site Name', choices=['bucharest'])
+    parser.add_argument('data_path', nargs='+', help='Data path.')
     parser.add_argument('--start', type=str, metavar='YYYY-MM-DD', help='Starting date. Default is current day - 7.',
                         default=process_utils.get_date_from_past(7))
     parser.add_argument('--stop', type=str, metavar='YYYY-MM-DD', help='Stopping date. Default is current day - 1.',
                         default=process_utils.get_date_from_past(1))
-    parser.add_argument('--output', type=str, metavar='/FOO/BAR', help='Output folder path. '
-                                                                       'Overrides config/main.ini value.')
     parser.add_argument('-o', '--overwrite', dest='overwrite', action='store_true',
                         help='Overwrites existing images', default=False)
     ARGS = parser.parse_args()
