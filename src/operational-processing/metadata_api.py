0f229593b89912d72c13b07bd9a2b0920ddc50b6
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-05-13 15:29:00 +0300

    Add an option to freeze a file in metadata API

--- src/operational-processing/metadata_api.py
+++ src/operational-processing/metadata_api.py
@@ -7,9 +7,12 @@ class MetadataApi:
         self.__url = url
         self.__session = session
 
-    def put(self, uuid, filepath):
+    def put(self, uuid, filepath, freeze=False):
         payload = subprocess.check_output(['ncdump', '-xh', filepath])
         url = f'{self.__url}file/{uuid}'
-        r = self.__session.put(url, data=payload)
+        headers = { 'Content-Type': 'application/xml' }
+        if freeze:
+            headers['X-Freeze'] = 'True'
+        r = self.__session.put(url, data=payload, headers=headers)
         r.raise_for_status()
         return r
--- tests/unit/test_metadata_api.py
+++ tests/unit/test_metadata_api.py
@@ -11,15 +11,27 @@ session.mount('mock://', adapter)
 class TestMetadataApi:
 
     def test_put_metadata(self):
-        def is_valid_xml(request):
-            try:
-                minidom.parseString(request.text)
-            except (TypeError, AttributeError):
-                return False
-            return True
-
-        adapter.register_uri('PUT', 'mock://test/file/uuid', additional_matcher=is_valid_xml, text='resp')
+        adapter.register_uri('PUT', 'mock://test/file/uuid', additional_matcher=self.__is_valid_xml, text='resp')
         md_api = metadata_api.MetadataApi('mock://test/', session)
         r = md_api.put('uuid', 'tests/data/output/bucharest/calibrated/chm15k/2020/20200118_bucharest_chm15k.nc')
             
         assert r.text == 'resp'
+
+    def test_put_metadata_freeze(self):
+        def has_freeze_in_header(request):
+            if not self.__is_valid_xml(request):
+                return False
+            return 'X-Freeze' in request.headers
+
+        adapter.register_uri('PUT', 'mock://test/file/uuid', additional_matcher=has_freeze_in_header, text='resp')
+        md_api = metadata_api.MetadataApi('mock://test/', session)
+        r = md_api.put('uuid', 'tests/data/output/bucharest/calibrated/chm15k/2020/20200118_bucharest_chm15k.nc', freeze=True)
+            
+        assert r.text == 'resp'
+
+    def __is_valid_xml(self, request):
+        try:
+            minidom.parseString(request.text)
+        except (TypeError, AttributeError):
+            return False
+        return True
