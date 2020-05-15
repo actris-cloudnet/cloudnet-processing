6b35e58ee48bffc282ed300463783356f788993a
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-05-15 16:28:08 +0300

    Refactor metadata api tests

--- tests/unit/test_metadata_api.py
+++ tests/unit/test_metadata_api.py
@@ -7,12 +7,14 @@ metadata_api = __import__('operational_processing').metadata_api
 adapter = requests_mock.Adapter()
 session = requests.Session()
 session.mount('mock://', adapter)
+mock_addr = 'mock://test/'
+
 
 class TestMetadataApi:
 
     def test_put_metadata(self):
-        adapter.register_uri('PUT', 'mock://test/file/uuid', additional_matcher=self.__is_valid_xml, text='resp')
-        md_api = metadata_api.MetadataApi('mock://test/', session)
+        adapter.register_uri('PUT', f'{mock_addr}file/uuid', additional_matcher=self.__is_valid_xml, text='resp')
+        md_api = metadata_api.MetadataApi(mock_addr, session)
         r = md_api.put('uuid', 'tests/data/output_fixed/bucharest/calibrated/chm15k/2020/20200118_bucharest_chm15k.nc')
             
         assert r.text == 'resp'
@@ -23,9 +25,10 @@ class TestMetadataApi:
                 return False
             return 'X-Freeze' in request.headers
 
-        adapter.register_uri('PUT', 'mock://test/file/uuid', additional_matcher=has_freeze_in_header, text='resp')
-        md_api = metadata_api.MetadataApi('mock://test/', session)
-        r = md_api.put('uuid', 'tests/data/output_fixed/bucharest/calibrated/chm15k/2020/20200118_bucharest_chm15k.nc', freeze=True)
+        adapter.register_uri('PUT', f'{mock_addr}file/uuid', additional_matcher=has_freeze_in_header, text='resp')
+        md_api = metadata_api.MetadataApi(mock_addr, session)
+        r = md_api.put('uuid', 'tests/data/output_fixed/bucharest/calibrated/chm15k/2020/20200118_bucharest_chm15k.nc',
+                       freeze=True)
             
         assert r.text == 'resp'
 
