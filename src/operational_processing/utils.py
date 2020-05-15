3518c41fc0308ccef6ab208426eba255cb011dd7
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-05-15 16:24:14 +0300

    Remove duplicate function

--- src/operational_processing/utils.py
+++ src/operational_processing/utils.py
@@ -80,24 +80,3 @@ def read_conf(args):
 
 def str2bool(s):
     return False if s == 'False' else True if s == 'True' else s
-
-
-def wait_for_port(port, host='localhost', timeout=10.0):
-    """Wait until a port starts accepting TCP connections. Used in e2e-tests.
-    Args:
-        port (int): Port number.
-        host (str): Host address on which the port should exist.
-        timeout (float): In seconds. How long to wait before raising errors.
-    Raises:
-        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
-    """
-    start_time = time.perf_counter()
-    while True:
-        try:
-            with socket.create_connection((host, port), timeout=timeout):
-                break
-        except OSError as ex:
-            time.sleep(0.01)
-            if time.perf_counter() - start_time >= timeout:
-                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
-                                   'connections.'.format(port, host)) from ex
