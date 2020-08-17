00c1e4f0859f9b18ab2315d8bf0292467759afe9
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-08-17 14:20:30 +0300

    Refactor

--- src/data_processing/data_submission_api.py
+++ src/data_processing/data_submission_api.py
@@ -1,6 +1,7 @@
 """Module defining data submission HTTP API routes."""
 import shutil
 import requests
+import os
 from os import path
 from collections import namedtuple
 from fastapi import FastAPI, BackgroundTasks, File, UploadFile, HTTPException
@@ -15,17 +16,11 @@ CONFIG = process_utils.read_conf(namedtuple('args', 'config_dir')('./config'))
 async def create_upload_file(background_tasks: BackgroundTasks, hashSum: str,
                              file_submitted: UploadFile = File(...)):
     """Submit raw measurement file to Cloudnet data portal."""
-
     local_filename = file_submitted.filename
     _save_file_to_disk(file_submitted, local_filename)
-
     metadata = _read_metadata(hashSum)
-
-    if not _hashes_match(hashSum, local_filename):
-        raise HTTPException(status_code=400, detail="Submitted file incompatible with the given hash")
-
+    _check_hash(hashSum, local_filename)
     background_tasks.add_task(_process, local_filename, metadata)
-
     return {"metadata": metadata}
 
 
@@ -44,15 +39,26 @@ def _read_metadata(hash_sum: str) -> dict:
     return res.json()
 
 
-def _hashes_match(hash_sum: str, file_local: str) -> bool:
+def _check_hash(hash_sum: str, file_local: str) -> None:
     hash_local = process_utils.sha256sum(file_local)
-    return hash_local == hash_sum
+    if hash_local != hash_sum:
+        raise HTTPException(status_code=400,
+                            detail="Submitted file incompatible with the given hash")
 
 
 def _process(filename: str, metadata: dict):
-    site = metadata['site']['id']
-    date = metadata['measurementDate']
-    product = metadata['product']['id']
-    if metadata['product'] == 'radar':
-        pass
+    _move_file_to_correct_folder(metadata, filename)
 
+
+def _move_file_to_correct_folder(metadata: dict, filename: str) -> None:
+    site = metadata['site']['id']
+    product = metadata['product']['id']
+    yyyy, mm, dd = metadata['measurementDate'].split('-')
+    if product == 'radar':
+        instrument = 'rpg-fmcw-94'
+    elif product == 'lidar':
+        instrument = 'chm15k'
+    root = CONFIG['main']['PATH']['output']
+    folder = path.join(root, site, 'uncalibrated', instrument, yyyy, mm, dd)
+    os.makedirs(folder, exist_ok=True)
+    shutil.move(filename, folder)
