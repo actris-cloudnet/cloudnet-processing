851d223943338c7d9cde47717c2e9f55e3763ce5
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-08-18 13:54:59 +0300

    Add e2e-script for data submission

---
+++ tests/e2e/data_submission/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import subprocess
+from os import path
+import time
+import shutil
+import requests
+from test_utils.utils import start_server, remove_dirs
+import signal
+
+SCRIPT_PATH = path.dirname(path.realpath(__file__))
+
+test_file = {
+    'name': 'chm15k_20200405.nc',
+    'hash': '1ed679842745d1b1809c63d203a0c2bfd2f2b43e15123b306ea9f7ec4c089b71'
+}
+
+
+def main():
+
+    web_server = subprocess.Popen(['python3', 'scripts/run-data-submission-api.py',
+                                   f"--config-dir=tests/data/config"])
+    time.sleep(3)
+
+    # Example input data for upload
+    shutil.copyfile(f"tests/data/input/{test_file['name']}",
+                    f"tests/data/api_input/{test_file['name']}")
+    remove_dirs('tests/data/input/granada/')
+
+    url = f"http://localhost:5700/data/{test_file['hash']}"
+    full_path = f"tests/data/api_input/{test_file['name']}"
+    files = {'file_submitted': (full_path,
+                                open(full_path, 'rb'),
+                                'application/x-netcdf',
+                                {'Content-Type': 'multipart/form-data', 'accept': 'application/json'})
+             }
+
+    start_server(5000, 'tests/data/server/metadata/', f'{SCRIPT_PATH}/md.log')
+
+    res = requests.post(url, files=files)
+    print(res.status_code, res.text)
+    remove_dirs('tests/data/input/granada/')
+
+    web_server.send_signal(signal.SIGINT)
+
+
+if __name__ == "__main__":
+    main()
