040c8531fbf3dcee9e3d48b75c619c919f901a60
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-08-20 12:23:19 +0300

    POST data file with metadata

--- scripts/run-data-submission-api.py
+++ scripts/run-data-submission-api.py
@@ -4,12 +4,16 @@ import os
 import shutil
 import subprocess
 from os import path
+import hashlib
+import ntpath
 import requests
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks, File, UploadFile, HTTPException
+from fastapi import FastAPI, BackgroundTasks, File, Form, UploadFile, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from data_processing import utils as process_utils
 
 app = FastAPI()
+security = HTTPBasic()
 
 parser = argparse.ArgumentParser(description='Run Cloudnet data submission HTTP API.')
 parser.add_argument('--config-dir', type=str, metavar='/FOO/BAR',
@@ -18,79 +22,83 @@ parser.add_argument('--config-dir', type=str, metavar='/FOO/BAR',
 ARGS = parser.parse_args()
 
 
-@app.put("/data/{hashSum}")
-async def create_upload_file(background_tasks: BackgroundTasks, hashSum: str,
-                             file_submitted: UploadFile = File(...)):
-    """Submit raw measurement file to Cloudnet data portal."""
-    local_filename = file_submitted.filename
-    _save_file_to_disk(file_submitted, local_filename)
-    metadata = _read_metadata(hashSum)
-    _check_hash(hashSum, local_filename)
-    background_tasks.add_task(_process, local_filename, metadata)
+@app.post("/data/")
+async def create_upload_file(background_tasks: BackgroundTasks,
+                             credentials: HTTPBasicCredentials = Depends(security),
+                             file_submitted: UploadFile = File(...), hashSum: str = Form(...),
+                             measurementDate: str = Form(...), product: str = Form(...)):
+    """Submit file with metadata to Cloudnet data portal."""
+    meta = {'hashSum': hashSum, 'measurementDate': measurementDate, 'product': product,
+            'filename': ntpath.basename(file_submitted.filename)}
+    _check_hash(hashSum, file_submitted)
+    _put_metadata(meta, credentials)
+    meta['site'] = credentials.username
+    background_tasks.add_task(_process, file_submitted, meta)
     return {"message": "File submission successful!"}
 
 
-def _save_file_to_disk(file_submitted: UploadFile, filename: str) -> None:
-    file = open(filename, 'wb+')
-    shutil.copyfileobj(file_submitted.file, file)
-    file.close()
+def _check_hash(reference_hash: str, file_obj: UploadFile) -> None:
+    hash_sum = hashlib.sha256()
+    for byte_block in iter(lambda: file_obj.file.read(4096), b""):
+        hash_sum.update(byte_block)
+    file_obj.file.seek(0)
+    if hash_sum.hexdigest() != reference_hash:
+        raise HTTPException(status_code=400, detail="Unexpected hash in the submitted file")
 
 
-def _read_metadata(hash_sum: str) -> dict:
+def _put_metadata(meta: dict, credentials) -> None:
     root = _read_conf()['main']['METADATASERVER']['url']
-    url = path.join(root, 'metadata', hash_sum)
-    res = requests.get(url)
-    if str(res.status_code) != '200':
-        raise HTTPException(status_code=404, detail="Metadata not found")
-    metadata = res.json()
-    if metadata['status'] == 'uploaded':
+    url = path.join(root, 'protected', 'metadata', meta['hashSum'])
+    meta['status'] = 'uploaded'
+    res = requests.put(url, data=meta, auth=(credentials.username, credentials.password))
+    if str(res.status_code) == '200':
         raise HTTPException(status_code=200, detail="File already exists")
-    _post_hash(root, hash_sum)
-    return metadata
+    if str(res.status_code) != '201':
+        raise HTTPException(status_code=int(res.status_code), detail="Error in metadata submission")
 
 
-def _post_hash(root: str, hash_sum: str) -> None:
+def _post_hash(hash_sum: str) -> None:
+    root = _read_conf()['main']['METADATASERVER']['url']
     url = path.join(root, 'protected', 'metadata', hash_sum)
     requests.post(url, data={'status': 'uploaded'})
+
+
+def _process(file_submitted: UploadFile, metadata: dict) -> None:
+    config = _read_conf(metadata['site'])
+    partial_path, full_path = _create_folder(config, metadata)
+    _save_file(file_submitted, full_path)
+    _post_hash(metadata['hashSum'])
+    if 'chm15k' in partial_path:
+        subprocess.check_call(['python3', 'scripts/concat-lidar.py', partial_path])
+    start_date = metadata['measurementDate']
+    stop_date = process_utils.get_date_from_past(-1, start_date)
+    subprocess.check_call(['python3', 'scripts/process-cloudnet.py', metadata['site'],
+                           f"--start={start_date}", f"--stop={stop_date}"])
+    output_path = path.join(config['main']['PATH']['output'], metadata['site'])
+    subprocess.check_call(['python3', 'scripts/plot-quicklooks.py', output_path])
+
+
+def _create_folder(config: dict, metadata: dict) -> (str, str):
+    product = metadata['product']
+    instrument = config['site']['INSTRUMENTS'][product]
+    root = path.join(config['main']['PATH']['input'], metadata['site'], 'uncalibrated', instrument)
+    yyyy, mm, dd = metadata['measurementDate'].split('-')
+    full_path = path.join(root, yyyy, mm, dd)
+    os.makedirs(full_path, exist_ok=True)
+    return root, full_path
+
+
+def _save_file(file_submitted: UploadFile, folder: str) -> None:
+    filename = ntpath.basename(file_submitted.filename)
+    file = open(path.join(folder, filename), 'wb+')
+    shutil.copyfileobj(file_submitted.file, file)
+    file.close()
 
 
 def _read_conf(site=None) -> dict:
     if site:
         ARGS.site = (site, )
     return process_utils.read_conf(ARGS)
-
-
-def _check_hash(hash_sum: str, file_local: str) -> None:
-    hash_local = process_utils.sha256sum(file_local)
-    if hash_local != hash_sum:
-        raise HTTPException(status_code=400,
-                            detail="Unexpected hash in the submitted file")
-
-
-def _process(filename: str, metadata: dict) -> None:
-    info = _move_file_to_correct_folder(metadata, filename)
-    if 'chm15k' in info['raw_data_path']:
-        subprocess.check_call(['python3', 'scripts/concat-lidar.py', info['raw_data_path']])
-    stop_date = process_utils.get_date_from_past(-1, info['date'])
-    subprocess.check_call(['python3', 'scripts/process-cloudnet.py', metadata['site']['id'],
-                           f"--start={info['date']}", f"--stop={stop_date}"])
-    output_path = path.join(info['config']['main']['PATH']['output'], metadata['site']['id'])
-    subprocess.check_call(['python3', 'scripts/plot-quicklooks.py', output_path])
-
-
-def _move_file_to_correct_folder(metadata: dict, filename: str) -> dict:
-    site = metadata['site']['id']
-    product = metadata['product']['id']
-    config = _read_conf(site)
-    instrument = config['site']['INSTRUMENTS'][product]
-    root = path.join(config['main']['PATH']['input'], site, 'uncalibrated', instrument)
-    yyyy, mm, dd = metadata['measurementDate'].split('-')
-    folder = path.join(root, yyyy, mm, dd)
-    os.makedirs(folder, exist_ok=True)
-    shutil.move(filename, folder)
-    return {'config': config,
-            'raw_data_path': root,
-            'date': metadata['measurementDate']}
 
 
 if __name__ == "__main__":
