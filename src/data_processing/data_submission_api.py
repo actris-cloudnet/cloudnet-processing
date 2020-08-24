4c4094300a05d1f29579780a71ce27f9bfe6cb28
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-08-24 11:32:13 +0300

    Use new directory tree

--- src/data_processing/data_submission_api.py
+++ src/data_processing/data_submission_api.py
@@ -11,18 +11,32 @@ from fastapi import UploadFile, HTTPException
 class DataSubmissionApi:
     """Class handling data submissions to Cloudnet data portal."""
 
-    def __init__(self, configs):
-        self.configs = configs
+    def __init__(self, config):
+        self.config = config
+        self._meta_server = self.config['METADATASERVER']['url']
 
     def put_metadata(self, meta: dict) -> None:
-        site = meta['site']
-        root = self.configs[site]['main']['METADATASERVER']['url']
-        url = path.join(root, 'metadata', meta['hashSum'])
+        url = path.join(self._meta_server, 'metadata', meta['hashSum'])
         res = requests.put(url, json=meta)
         if str(res.status_code) == '200':
             raise HTTPException(status_code=200, detail="File already exists")
         if str(res.status_code) != '201':
             raise HTTPException(status_code=int(res.status_code), detail=res.json())
+
+    def post_hash(self, meta: dict) -> None:
+        url = path.join(self._meta_server, 'metadata', meta['hashSum'])
+        requests.post(url, json={'status': 'uploaded'})
+
+    def save_file(self, meta: dict, file_obj: UploadFile) -> None:
+        folder = self._create_folder(meta)
+        self._save(file_obj, folder)
+
+    def _create_folder(self, meta: dict) -> (str, str):
+        root = path.join(self.config['PATH']['api_files'], meta['site'], meta['instrument'])
+        yyyy, mm, dd = meta['measurementDate'].split('-')
+        full_path = path.join(root, yyyy, mm, dd)
+        os.makedirs(full_path, exist_ok=True)
+        return full_path
 
     @staticmethod
     def check_hash(meta: dict, file_obj: UploadFile) -> None:
@@ -32,26 +46,6 @@ class DataSubmissionApi:
         file_obj.file.seek(0)
         if hash_sum.hexdigest() != meta['hashSum']:
             raise HTTPException(status_code=400, detail="hashSum does not match sent file")
-
-    def post_hash(self, meta: dict) -> None:
-        site = meta['site']
-        root = self.configs[site]['main']['METADATASERVER']['url']
-        url = path.join(root, 'metadata', meta['hashSum'])
-        requests.post(url, json={'status': 'uploaded'})
-
-    def save_file(self, meta: dict, file_obj: UploadFile) -> None:
-        folder = self._create_folder(meta)
-        self._save(file_obj, folder)
-
-    def _create_folder(self, meta: dict) -> (str, str):
-        site = meta['site']
-        product = meta['product']
-        instrument = self.configs[site]['site']['INSTRUMENTS'][product]
-        root = path.join(self.configs[site]['main']['PATH']['input'], site, 'uncalibrated', instrument)
-        yyyy, mm, dd = meta['measurementDate'].split('-')
-        full_path = path.join(root, yyyy, mm, dd)
-        os.makedirs(full_path, exist_ok=True)
-        return full_path
 
     @staticmethod
     def _save(file_obj: UploadFile, folder: str) -> None:
