2af3f843702eb73689cf872b742dc1a2ab53ebd9
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-11-13 14:06:35 +0200

    Refactor chm15k concatenation

--- tests/e2e/concat_lidar/main.py
+++ tests/e2e/concat_lidar/main.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
-import subprocess
+import os
 import pytest
-from test_utils.utils import remove_files, remove_dir
+import glob
+from data_processing.concat_lib import concat_chm15k_files
 
-YEAR = 2020
 
 def main():
 
-    def _get_result_path(path_in):
-        return f'{path_in}/2020/'
+    fpath = 'tests/data/input/bucharest/uncalibrated/chm15k/2020/04/02/'
 
-    # Test default output path
-    data_path = 'tests/data/input/bucharest/uncalibrated/chm15k/'
-    result_path = _get_result_path(data_path)
-    remove_files(result_path)
-    subprocess.check_call(['python3', 'scripts/concat-lidar.py', data_path])
-    pytest.main(['-v', 'tests/e2e/concat_lidar/tests.py',
-                 '-k', 'TestDefaultProcessing',
-                 '--input', data_path])
-    remove_files(result_path)
+    output_file = f'{fpath}concatenated_chm15k_file.nc'
+    if os.path.isfile(output_file):
+        os.remove(output_file)
 
-    # Test user-defined output path
-    output = 'tests/data/output/'
-    result_path = _get_result_path(output)
-    remove_dir(result_path)
-    subprocess.check_call(['python3', 'scripts/concat-lidar.py', data_path,
-                           f'--output={output}'])
+    chm15k_files = glob.glob(f'{fpath}*nc',)
+
+    concat_chm15k_files(chm15k_files, '2020-04-02', output_file)
+
     pytest.main(['-v', 'tests/e2e/concat_lidar/tests.py',
-                 '-k', 'TestOutput',
-                 '--output', output])
-    remove_dir(result_path)
+                 '--output', output_file])
 
 
 if __name__ == "__main__":
--- tests/e2e/concat_lidar/tests.py
+++ tests/e2e/concat_lidar/tests.py
@@ -2,38 +2,27 @@ import glob
 import pytest
 import netCDF4
 from cloudnetpy.utils import seconds2hours
+import os
 
 YEAR = 2020
 N_VALID_FILES = 2
 N_TIME_IN_SINGLE_FILE = 10
 
 
-class TestDefaultProcessing:
+class TestCHM15kConcatenation:
 
     @pytest.fixture(autouse=True)
     def _fetch_params(self, params):
-        self._path = params['input']
-        self._files = glob.glob(f'{self._path}/{YEAR}/*.nc')
+        self._output = params['output']
 
     def test_file_arrived(self):
-        assert len(self._files) == 1
+        assert os.path.isfile(self._output)
 
     def test_time_vector_is_correct(self):
-        nc = netCDF4.Dataset(self._files[0])
+        nc = netCDF4.Dataset(self._output)
         time = nc.variables['time'][:]
         assert len(time) == N_VALID_FILES * N_TIME_IN_SINGLE_FILE
         fraction_hour = seconds2hours(time)
         assert min(fraction_hour) > 0
         assert max(fraction_hour) < 24
         nc.close()
-
-
-class TestOutput:
-
-    @pytest.fixture(autouse=True)
-    def _fetch_params(self, params):
-        self._path = params['output']
-
-    def test_file_arrived(self, params):
-        files = glob.glob(f'{self._path}/{YEAR}/*.nc')
-        assert len(files) == 1
