223c97b5095f28419c7e8c6f609122528fbec337
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-12-11 14:55:01 +0200

    Upload legacy files from one site only

--- scripts/put-legacy-files.py
+++ scripts/put-legacy-files.py
@@ -2,7 +2,7 @@
 """Script for creating and putting missing images into s3 / database."""
 import argparse
 import os
-from pathlib import Path
+from pathlib import Path, PurePath
 from typing import Generator
 from tempfile import NamedTemporaryFile
 import netCDF4
@@ -23,55 +23,50 @@ def main():
     storage_api = StorageApi(config)
     pid_utils = PidUtils(config)
 
-    site_metadata = md_api.get('api/sites', {'modelSites': True})
-    sites = [site['id'] for site in site_metadata]
+    site = PurePath(ARGS.path[0]).name
 
-    for site in sites:
-        dir_names = [
-            'processed/categorize/',
-            'products/iwc-Z-T-method/',
-            'products/lwc-scaled-adiabatic/',
-            'products/drizzle/',
-            'products/classification/'
-        ]
-        for dir_name in dir_names:
-            files = _get_files(ARGS.root[0], site, dir_name)
+    dir_names = [
+        'processed/categorize/',
+        'products/iwc-Z-T-method/',
+        'products/lwc-scaled-adiabatic/',
+        'products/drizzle/',
+        'products/classification/'
+    ]
+    for dir_name in dir_names:
+        files = _get_files(ARGS.path[0], dir_name)
 
-            for file in files:
-                legacy_file = LegacyFile(file)
+        for file in files:
+            legacy_file = LegacyFile(file)
 
-                try:
-                    info = {
-                        'date_str': legacy_file.get_date_str(),
-                        'product': legacy_file.get_product_type(),
-                        'identifier': legacy_file.get_identifier(),
-                        'site': site
-                    }
-                    _check_if_exists(md_api, info)
-                except (MiscError, HTTPError) as err:
-                    print(err)
-                    continue
-                finally:
-                    legacy_file.close()
+            try:
+                info = {
+                    'date_str': legacy_file.get_date_str(),
+                    'product': legacy_file.get_product_type(),
+                    'identifier': legacy_file.get_identifier(),
+                    'site': site
+                }
+                _check_if_exists(md_api, info)
+            except (MiscError, HTTPError) as err:
+                print(err)
+                continue
+            finally:
+                legacy_file.close()
 
-                s3key = _get_s3key(info)
-                print(s3key)
+            s3key = _get_s3key(info)
+            print(s3key)
 
-                temp_file = NamedTemporaryFile()
-                uuid = fix_legacy_file(file, temp_file.name)
+            temp_file = NamedTemporaryFile()
+            uuid = fix_legacy_file(file, temp_file.name)
 
-                pid_utils.add_pid_to_file(temp_file.name)
-                upload_info = storage_api.upload_product(temp_file.name, s3key)
-                img_metadata = storage_api.create_and_upload_images(temp_file.name,
-                                                                    s3key,
-                                                                    uuid,
-                                                                    info['product'],
-                                                                    legacy=True)
-                payload = utils.create_product_put_payload(temp_file.name, upload_info)
-                payload['legacy'] = True
-                md_api.put(s3key, payload)
-                for data in img_metadata:
-                    md_api.put_img(data, uuid)
+            pid_utils.add_pid_to_file(temp_file.name)
+            upload_info = storage_api.upload_product(temp_file.name, s3key)
+            img_metadata = storage_api.create_and_upload_images(temp_file.name, s3key, uuid,
+                                                                info['product'], legacy=True)
+            payload = utils.create_product_put_payload(temp_file.name, upload_info)
+            payload['legacy'] = True
+            md_api.put(s3key, payload)
+            for data in img_metadata:
+                md_api.put_img(data, uuid)
 
 
 class LegacyFile:
@@ -119,29 +114,28 @@ def _check_if_exists(md_api, info: dict):
     except HTTPError as err:
         raise err
     if metadata:
-        raise MiscError('Legacy file already uploaded')
+        raise MiscError(f'{info["site"]} {info["date_str"]} {info["product"]} already uploaded')
 
 
 def _get_s3key(info: dict) -> str:
     return f"legacy/{info['date_str'].replace('-', '')}_{info['site']}_{info['identifier']}.nc"
 
 
-def _get_files(root_path: str, site: str, dir_name: str) -> Generator:
-    full_path = os.path.join(root_path, site, dir_name)
+def _get_files(root_path: str, dir_name: str) -> Generator:
+    full_path = os.path.join(root_path, dir_name)
     return Path(full_path).rglob('*.nc')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fix legacy files and upload to S3.')
-    parser.add_argument('root',
+    parser.add_argument('path',
                         nargs='+',
-                        help='Root path including the site directories, e.g., '
-                             '/ibrix/arch/dmz/cloudnet/data/')
+                        help='Path to the specific site directory containing legacy products '
+                             'in the subdirectories, e.g., /ibrix/arch/dmz/cloudnet/data/arm-sgp')
     parser.add_argument('--config-dir',
                         type=str,
                         metavar='/FOO/BAR',
                         help='Path to directory containing config files. Default: ./config.',
                         default='./config')
-
     ARGS = parser.parse_args()
     main()
