10c7632df59b1ea6ee8d8e550b120b114770d669
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-12-21 10:34:06 +0200

    Add again the largest file only option

--- scripts/process-cloudnet.py
+++ scripts/process-cloudnet.py
@@ -219,19 +219,27 @@ class Process:
                             raw_daily_file: str,
                             instrument: str = None,
                             model: str = None) -> Tuple[list, str]:
-        full_path, uuid = self._download_raw_data(instrument=instrument, model=model)
-        assert len(full_path) == 1 and len(uuid) == 1
+        full_path, uuid = self._download_raw_data(instrument=instrument, model=model,
+                                                  largest_file_only=True)
         shutil.move(full_path[0], raw_daily_file)
         original_filename = os.path.basename(full_path[0])
         return uuid, original_filename
 
-    def _download_raw_data(self, instrument: str = None, model: str = None) -> Tuple[list, list]:
+    def _download_raw_data(self,
+                           instrument: str = None,
+                           model: str = None,
+                           largest_file_only: bool = False) -> Tuple[list, list]:
         payload = self._get_payload()
         all_upload_metadata = self._md_api.get('upload-metadata', payload)
         upload_metadata = self._md_api.screen_metadata(all_upload_metadata,
                                                        instrument=instrument,
                                                        model=model)
         self._check_raw_data_status(upload_metadata)
+        if largest_file_only:
+            if len(upload_metadata) > 1:
+                print('Warning: several daily raw files (probably submitted without '
+                      '"allowUpdate")', end='\t')
+            upload_metadata = [upload_metadata[0]]
         full_paths = self._storage_api.download_raw_files(upload_metadata, self._temp_dir.name)
         uuids = [row['uuid'] for row in upload_metadata]
         return full_paths, uuids
