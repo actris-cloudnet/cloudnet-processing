0464e13eab12875460acd48e5b794ead68bbd86d
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-02-13 10:21:39 +0200

    Add test environment for pids

--- src/data_processing/pid_utils.py
+++ src/data_processing/pid_utils.py
@@ -8,23 +8,30 @@ class PidUtils:
 
     def __init__(self, config: dict):
         self._pid_service_url = config['PID-SERVICE']['url']
+        self._is_production = self._get_env(config)
+
+    @staticmethod
+    def _get_env(config) -> bool:
+        is_test_env = config['PID-SERVICE'].get('test_env', False)
+        return not bool(is_test_env)
 
     def add_pid_to_file(self, filepath: str) -> Tuple[str, str]:
         """Queries PID service and adds the PID to NC file metadata."""
         rootgrp = netCDF4.Dataset(filepath, 'r+')
         uuid = getattr(rootgrp, 'file_uuid')
-        payload = {
-            'type': 'file',
-            'uuid': uuid
-        }
-        res = requests.post(self._pid_service_url, json=payload)
-
-        try:
-            res.raise_for_status()
-        except HTTPError:
-            raise HTTPError(f'PID service failed with status {res.status_code}:\n{res.json()["detail"]}')
-
-        pid = res.json()['pid']
+        if self._is_production:
+            payload = {
+                'type': 'file',
+                'uuid': uuid
+            }
+            res = requests.post(self._pid_service_url, json=payload)
+            try:
+                res.raise_for_status()
+            except HTTPError:
+                raise HTTPError(f'PID service failed with status {res.status_code}:\n{res.json()["detail"]}')
+            pid = res.json()['pid']
+        else:
+            pid = 'https://www.example.pid/'
         rootgrp.pid = pid
         rootgrp.close()
 
