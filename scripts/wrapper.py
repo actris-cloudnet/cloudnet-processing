fcffb42065deb9eeef500dfe67e591db23ea4275
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-05-28 12:30:39 +0300

    Add wrapper for scripts

---
+++ scripts/wrapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+from data_processing import utils
+
+
+def main(args):
+
+    try:
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError as err:
+        utils.send_slack_alert(err, 'wrapper')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
