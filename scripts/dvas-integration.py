35448aed31accecf5ec4968bb0cbb611e2822bde
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-10-01 15:49:34 +0300

    Make request to dvas dp

--- scripts/dvas-integration.py
+++ scripts/dvas-integration.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import json
+import os
 from data_processing import utils
 from datetime import datetime
 import re
+import requests
 
 
 def create_title(file):
@@ -26,115 +27,127 @@ def parse_instrument_type(product):
     else:
         return ['radar', 'lidar', 'mwr']
 
-def main():
-    variables = utils.get_from_data_portal_api('api/products/variables')
-    stable = utils.get_from_data_portal_api('api/files', dict(volatile=False, updatedAtFrom='2020-01-01'))
-    file = stable[0]
-    site = file['site']
-    product = file['product']
-    file_vars = list(filter(lambda var: var['id'] == product['id'], variables))
-    var_ids = list(map(lambda var: re.sub(r'.*-', '', var['id']), file_vars[0]['variables']))
-    print(var_ids)
-    pid = { 'id': file['pid'].replace('https://hdl.handle.net/', ''), 'type': 'Handle' } if file['pid'] else { 'id': None, 'type': 'N/A' }
 
-    actris_json = {
-      'md_metadata': { # mandatory
-        'file_identifier': file['filename'],
-        'language': 'en', # mandatory
-        'hierarchy_level': 'dataset', # mandatory, fixed list ['attribute','attributeType','collectionHardware','collectionSession','dataset','series','nonGeographicDataset','dimensionGroup','feature','featureType','propertyType','fieldSession','software','service','model','tile']
-        'datestamp': datetime.now().isoformat(), # mandatory
-        'contact': [{ # mandatory
-          'first_name': 'Ewan', # mandatory
-          'last_name': 'O\'Connor', # mandatory
-          'organisation_name': 'Finnish Meteorological Institute (FMI)', # mandatory
-          'role_code': ['pointOfContact'], # mandatory, fixed list ['resourceProvider','custodian','owner,'user,'distributor,'originator,'pointOfContact,'principalInvestigator,'processor,'publisher,'author]
-          'country': 'Finland', # mandatory
-          'country_code': 'FI'
-        }],
-        'online_resource': { # mandatory
-          'linkage': 'https://cloudnet.fmi.fi/' # mandatory
+def main():
+    file1 = open('scripts/last-success', 'r')
+    lines = file1.readlines()
+    startdate = lines[0]
+    enddate = datetime.now().isoformat()
+
+    variables = utils.get_from_data_portal_api('api/products/variables')
+
+    payload = dict(volatile=False, updatedAtFrom=startdate, updatedAtTo=enddate)
+    files = utils.get_from_data_portal_api('api/files', payload)
+
+    for file in files:
+        site = file['site']
+        product = file['product']
+        file_vars = list(filter(lambda var: var['id'] == product['id'], variables))
+        var_ids = list(map(lambda var: re.sub(r'.*-', '', var['id']), file_vars[0]['variables']))
+        pid = { 'id': file['pid'].replace('https://hdl.handle.net/', ''), 'type': 'Handle' } if file['pid'] else { 'id': None, 'type': 'N/A' }
+
+        actris_json = {
+          'md_metadata': { # mandatory
+            'file_identifier': file['filename'],
+            'language': 'en', # mandatory
+            'hierarchy_level': 'dataset', # mandatory, fixed list ['attribute','attributeType','collectionHardware','collectionSession','dataset','series','nonGeographicDataset','dimensionGroup','feature','featureType','propertyType','fieldSession','software','service','model','tile']
+            'datestamp': datetime.now().isoformat(), # mandatory
+            'contact': [{ # mandatory
+              'first_name': 'Ewan', # mandatory
+              'last_name': 'O\'Connor', # mandatory
+              'organisation_name': 'Finnish Meteorological Institute (FMI)', # mandatory
+              'role_code': ['pointOfContact'], # mandatory, fixed list ['resourceProvider','custodian','owner,'user,'distributor,'originator,'pointOfContact,'principalInvestigator,'processor,'publisher,'author]
+              'country': 'Finland', # mandatory
+              'country_code': 'FI'
+            }],
+            'online_resource': { # mandatory
+              'linkage': 'https://cloudnet.fmi.fi/' # mandatory
+            }
+          },
+          'md_identification': { # mandatory
+            'abstract': create_title(file), # mandatory
+            'title': create_title(file), # mandatory
+            'identifier': pid, # optional
+            'date': file['measurementDate'], # mandatory
+            'date_type': 'creation', # mandatory, fixed list ['publication', 'revision', 'creation'
+            'contact': [{ # mandatory
+              'first_name': 'Simo', # mandatory
+              'last_name': 'Tukiainen', # mandatory
+              'organisation_name': 'Finnish Meteorological Institute (FMI)', # mandatory
+              'role_code': ['processor'], # mandatory, see fixed list in example above
+              'country': 'Finland', # mandatory
+              'country_code': 'FI'
+            }],
+            'online_resource': { # mandatory
+              'linkage': f"https://cloudnet.fmi.fi/file/{file['uuid']}" # mandatory
+            }
+          },
+          'md_constraints': { # mandatory
+            'access_constraints': 'otherRestrictions', # mandatory
+            'use_constraints': 'otherRestrictions', # mandatory
+            'other_constraints': 'http://actris.nilu.no/Content/Documents/DataPolicy.pdf', # mandatory
+          },
+          'md_keywords': { # mandatory
+            'keywords': ['FMI', 'ACTRIS', product['humanReadableName']] # mandatory, limit on 60 character keyword
+          },
+          'md_data_identification': { # mandatory
+            'language': 'en', # mandatory
+            'topic_category': 'climatologyMeteorologyAtmosphere', # mandatory
+            'description': 'time series of point measurements', # mandatory
+            'station_identifier': 'INO' # mandatory, fixed list will be provided
+          },
+          'ex_geographic_bounding_box': { # mandatory
+            'west_bound_longitude': site['longitude'], # mandatory
+            'east_bound_longitude': site['longitude'], # mandatory
+            'south_bound_latitude': site['latitude'], # mandatory
+            'north_bound_latitude': site['latitude'] # mandatory
+          },
+          'ex_temporal_extent': { # mandatory
+            'time_period_begin': file['measurementDate'], # mandatory
+            'time_period_end': file['measurementDate'] # mandatory
+          },
+          'ex_vertical_extent': { # optional
+            'minimum_value': None, # optional
+            'maximum_value': None, # optional
+            'unit_of_measure': 'm above sea level' # optional
+          },
+          'md_content_information': { # mandatory
+            'attribute_descriptions': list(map(lambda var: re.sub(r'[-_]/g', '.', var), var_ids)), # mandatory, list of parameters
+            'content_type': 'physicalMeasurement' # mandatory, fixed list ['image','thematicClassification','physicalMeasurement']
+          },
+          'md_distribution_information': { # mandatory
+            'data_format': file['format'], # mandatory
+            'version_data_format': file['format'], # mandatory
+            'transfersize': file['size'], # optional
+            'dataset_url': file['downloadUrl'], # mandatory
+            'protocol': 'http', # mandatory, fixed list ['http','opendap']
+            'description': 'Direct download of data file', # optional
+            'function': 'download', # mandatory
+            'restriction': {
+              'set': False, # mandatory
+            }
+          },
+          'md_actris_specific': { # mandatory
+            'platform_type': 'surface_station', # mandatory ["surface_station", "simulation_chamber", "ballon"]
+            'product_type': 'model' if product['id'] == 'model' else 'observation', # mandatory ["model", "observation", "fundamental_parameter"]
+            'matrix': 'cloud', # mandatory ["cloud", "gas", "particle", "met"]
+            'sub_matrix': 'Unknown', # mandatory
+            'instrument_type': parse_instrument_type(product), # mandatory
+            'program_affiliation': parse_affiliation(site), # mandatory, fixed list ['ACTRIS', 'AMAP', 'AMAP_public','EUSAAR','EMEP','ACTRIS_preliminary','GAW-WDCA','GAW-WDCRG','NOAA-ESRL']
+            'legacy_data': file['legacy'], # mandatory
+            'data_level': product['level'][0], # mandatory, fixed list [0, 1, 2, 3]
+            'data_sublevel': product['level'][1] if 1 < len(product['level']) else None, # optional
+            'data_product': 'near-realtime-data' if file['quality'] == 'nrt' else 'quality assured data' # mandatory, need fixed list e.g. ['higher level data','quality assured data', 'near-realtime-data']
+          },
+          'dq_data_quality_information': { # optional
+            'level': 'dataset', # optional, fixed list ['attribute', 'attributeType', 'collectionHardware', 'collectionSession', 'dataset', 'series', 'nonGeographicDataset', 'dimensionGroup', 'feature', 'featureType', 'propertyType', 'fieldSession', 'software', 'service', 'model', 'tile']
+          },
         }
-      },
-      'md_identification': { # mandatory
-        'abstract': create_title(file), # mandatory
-        'title': create_title(file), # mandatory
-        'identifier': pid, # optional
-        'date': file['measurementDate'], # mandatory
-        'date_type': 'creation', # mandatory, fixed list ['publication', 'revision', 'creation'
-        'contact': [{ # mandatory
-          'first_name': 'Simo', # mandatory
-          'last_name': 'Tukiainen', # mandatory
-          'organisation_name': 'Finnish Meteorological Institute (FMI)', # mandatory
-          'role_code': ['processor'], # mandatory, see fixed list in example above
-          'country': 'Finland', # mandatory
-          'country_code': 'FI'
-        }],
-        'online_resource': { # mandatory
-          'linkage': f"https://cloudnet.fmi.fi/file/{file['uuid']}" # mandatory
-        }
-      },
-      'md_constraints': { # mandatory
-        'access_constraints': 'otherRestrictions', # mandatory
-        'use_constraints': 'otherRestrictions', # mandatory
-        'other_constraints': 'http://actris.nilu.no/Content/Documents/DataPolicy.pdf', # mandatory
-      },
-      'md_keywords': { # mandatory
-        'keywords': ['FMI', 'ACTRIS', product['humanReadableName']] # mandatory, limit on 60 character keyword
-      },
-      'md_data_identification': { # mandatory
-        'language': 'en', # mandatory
-        'topic_category': 'climatologyMeteorologyAtmosphere', # mandatory
-        'description': 'time series of point measurements', # mandatory
-        'station_identifier': 'INO' # mandatory, fixed list will be provided
-      },
-      'ex_geographic_bounding_box': { # mandatory
-        'west_bound_longitude': site['longitude'], # mandatory
-        'east_bound_longitude': site['longitude'], # mandatory
-        'south_bound_latitude': site['latitude'], # mandatory
-        'north_bound_latitude': site['latitude'] # mandatory
-      },
-      'ex_temporal_extent': { # mandatory
-        'time_period_begin': file['measurementDate'], # mandatory
-        'time_period_end': file['measurementDate'] # mandatory
-      },
-      'ex_vertical_extent': { # optional
-        'minimum_value': None, # optional
-        'maximum_value': None, # optional
-        'unit_of_measure': 'm above sea level' # optional
-      },
-      'md_content_information': { # mandatory
-        'attribute_descriptions': list(map(lambda var: re.sub(r'[-_]/g', '.', var), var_ids)), # mandatory, list of parameters
-        'content_type': 'physicalMeasurement' # mandatory, fixed list ['image','thematicClassification','physicalMeasurement']
-      },
-      'md_distribution_information': { # mandatory
-        'data_format': file['format'], # mandatory
-        'version_data_format': file['format'], # mandatory
-        'transfersize': file['size'], # optional
-        'dataset_url': file['downloadUrl'], # mandatory
-        'protocol': 'http', # mandatory, fixed list ['http','opendap']
-        'description': 'Direct download of data file', # optional
-        'function': 'download', # mandatory
-        'restriction': {
-          'set': False, # mandatory
-        }
-      },
-      'md_actris_specific': { # mandatory
-        'platform_type': 'surface_station', # mandatory ["surface_station", "simulation_chamber", "ballon"]
-        'product_type': 'model' if product['id'] == 'model' else 'observation', # mandatory ["model", "observation", "fundamental_parameter"]
-        'matrix': 'cloud', # mandatory ["cloud", "gas", "particle", "met"]
-        'sub_matrix': 'Unknown', # mandatory
-        'instrument_type': parse_instrument_type(product), # mandatory
-        'program_affiliation': parse_affiliation(site), # mandatory, fixed list ['ACTRIS', 'AMAP', 'AMAP_public','EUSAAR','EMEP','ACTRIS_preliminary','GAW-WDCA','GAW-WDCRG','NOAA-ESRL']
-        'legacy_data': file['legacy'], # mandatory
-        'data_level': product['level'][0], # mandatory, fixed list [0, 1, 2, 3]
-        'data_sublevel': product['level'][1] or None, # optional
-        'data_product': 'near-realtime-data' if file['quality'] == 'nrt' else 'quality assured data' # mandatory, need fixed list e.g. ['higher level data','quality assured data', 'near-realtime-data']
-      },
-      'dq_data_quality_information': { # optional
-        'level': 'dataset', # optional, fixed list ['attribute', 'attributeType', 'collectionHardware', 'collectionSession', 'dataset', 'series', 'nonGeographicDataset', 'dimensionGroup', 'feature', 'featureType', 'propertyType', 'fieldSession', 'software', 'service', 'model', 'tile']
-      },
-    }
-    print(json.dumps(actris_json, indent=4))
+
+        headers = {'X-Authorization': f"Bearer {os.environ['DVAS_PORTAL_TOKEN']}"}
+        res = requests.post(f"{os.environ['DVAS_PORTAL_URL']}/Metadata/add", json=actris_json, headers=headers)
+        print(res.json())
+        res.raise_for_status()
 
 
 
