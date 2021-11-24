f2fb1712b7a9925e6e9a548665b30a7ff0ff82ba
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-11-24 10:15:38 +0200

    Add script for processing latest l3 prods

---
+++ scripts/process-latest-l3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import subprocess
+from datetime import datetime, timedelta
+from data_processing import utils
+
+sites = utils.get_cloudnet_sites()
+
+interpreter = 'python3'
+script = 'scripts/process-model-evaluation.py'
+wrapper = 'scripts/wrapper.py'
+products = 'l3-cf,l3-iwc,l3-lwc'
+
+for site in sites:
+    date = datetime.now() - timedelta(3)
+    date = datetime.strftime(date, '%Y-%m-%d')
+    subprocess.check_call([interpreter, wrapper, interpreter, script, site, '-p', products,
+                           '--start', date])
