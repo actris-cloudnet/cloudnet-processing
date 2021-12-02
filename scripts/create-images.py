d01477363e8caef2caceaebf706857a44482cf6f
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-12-02 11:25:39 +0200

    Remove obsolete imports

--- scripts/create-images.py
+++ scripts/create-images.py
@@ -9,7 +9,6 @@ import argparse
 from tempfile import TemporaryDirectory
 from data_processing.utils import read_main_conf
 from data_processing import metadata_api
-from data_processing.pid_utils import PidUtils
 from data_processing.storage_api import StorageApi
 from data_processing import utils
 from data_processing.processing_tools import ProcessBase
