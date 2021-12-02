e553bc329e4cc952f2acc3110328bc4efb0a7d0c
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-12-02 11:00:21 +0200

    Add script template

---
+++ scripts/create-qc-reports.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""A script for creating Cloudnet quality control (QC) reports."""
+import sys
+import os
+import requests
+import glob
+import logging
+import argparse
+from tempfile import TemporaryDirectory
+from data_processing.utils import read_main_conf
+from data_processing import metadata_api
+from data_processing.storage_api import StorageApi
+from data_processing import utils
+from requests.exceptions import HTTPError
+
+
+def main(args, storage_session=requests.session()):
+    args = _parse_args(args)
+    utils.init_logger(args)
+    config = read_main_conf()
+    md_api = metadata_api.MetadataApi(config, requests.session())
+    storage_api = StorageApi(config, storage_session)
+    metadata = md_api.find_files_for_plotting(args)
+    temp_dir_root = utils.get_temp_dir(config)
+    temp_dir = TemporaryDirectory(dir=temp_dir_root)
+    for row in metadata:
+        date_str = row['measurementDate']
+        product = row['product']['id']
+        site = row['site']['id']
+        logging.info(f'Plotting images for: {site} - {date_str} - {product}')
+        try:
+            full_path = storage_api.download_product(row, temp_dir.name)
+        except HTTPError as err:
+            logging.error(err)
+            continue
+        try:
+            # Do the qc
+            pass
+        except OSError as err:
+            logging.error(err)
+        for filename in glob.glob(f'{temp_dir.name}/*'):
+            os.remove(filename)
+
+
+def _parse_args(args):
+    parser = argparse.ArgumentParser(description='Create QC reports')
+    parser.add_argument('site', help='Site Name', type=str, default=None, nargs='?')
+    parser.add_argument('--start',
+                        type=str,
+                        metavar='YYYY-MM-DD',
+                        help='Starting date. Plot all dates by default.',
+                        default=None)
+    parser.add_argument('--stop',
+                        type=str,
+                        metavar='YYYY-MM-DD',
+                        help='Stopping date. Plot all dates by default.',
+                        default=None)
+    parser.add_argument('-d', '--date',
+                        type=str,
+                        metavar='YYYY-MM-DD',
+                        help='Single date to be plotted.')
+    parser.add_argument('-p', '--products',
+                        help='Products to be analyzed, e.g., radar,lidar,mwr,categorize,iwc \
+                              By default analyzes all products.',
+                        type=lambda s: s.split(','),
+                        default=None)
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
