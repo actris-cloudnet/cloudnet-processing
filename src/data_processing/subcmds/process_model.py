539209104baeb55a059176f6ff850583c1cb440e
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-03-26 01:10:41 +0200

    Refactor model processing script
    Refactor

--- src/data_processing/subcmds/process_model.py
+++ src/data_processing/subcmds/process_model.py
@@ -19,53 +19,49 @@ def main(args, storage_session: Optional[requests.Session] = None):
         storage_session = make_session()
     config = utils.read_main_conf()
     process = ProcessModel(args, config, storage_session=storage_session)
-    for date_str, model, raw_uuid in process.get_models_to_process():
-        process.date_str = date_str
-        logging.info(f'{args.site}, {date_str}, {model}')
+    for row in process.get_uploaded_model_metadata():
+        process.date_str = row["measurementDate"]
+        logging.info(f'{process.site}, {process.date_str}, {row["model"]["id"]}')
         uuid = Uuid()
         try:
-            uuid.volatile = process.fetch_volatile_model_uuid(model, raw_uuid)
-            uuid = process.process_model(uuid, model)
-            process.upload_product(process.temp_file.name, 'model', uuid, model)
-            process.create_and_upload_images(process.temp_file.name, 'model', uuid.product, model)
+            uuid.volatile = process.fetch_volatile_model_uuid(row)
+            uuid = process.process_model(uuid, row)
+            process.upload_product(process.temp_file.name, 'model', uuid, row["model"]["id"])
+            process.create_and_upload_images(process.temp_file.name, 'model', uuid.product, row["model"]["id"])
             process.upload_quality_report(process.temp_file.name, uuid.product)
             process.print_info()
         except MiscError as err:
             logging.warning(err)
         except (HTTPError, ConnectionError, RuntimeError) as err:
-            utils.send_slack_alert(err, 'model', args, date_str, model)
+            utils.send_slack_alert(err, 'model', args, process.date_str, row["model"]["id"])
 
 
 class ProcessModel(ProcessBase):
 
-    def get_models_to_process(self) -> list:
+    def get_uploaded_model_metadata(self) -> list:
         minimum_size = 20200
         payload = {
             'site': self.site,
             'status': 'uploaded'
         }
         metadata = self.md_api.get('upload-model-metadata', payload)
-        return [(row['measurementDate'], row['model']['id'], row['uuid']) for row in metadata
-                if int(row['size']) > minimum_size]
+        return [row for row in metadata if int(row['size']) > minimum_size]
 
-    def process_model(self, uuid: Uuid, model: str) -> Uuid:
-        payload = self._get_payload(model=model, skip_created=True)
-        upload_metadata = self.md_api.get('upload-model-metadata', payload)
-        self._check_raw_data_status(upload_metadata)
-        full_path, uuid.raw = self._download_raw_files(upload_metadata, self.temp_file)
+    def process_model(self, uuid: Uuid, metadata: dict) -> Uuid:
+        full_path, uuid.raw = self._download_raw_files([metadata], self.temp_file)
         data = {
             'site_name': self.site,
             'date': self.date_str,
             'uuid': uuid.volatile,
             'full_path': full_path,
-            'model': model,
+            'model': metadata["model"]["id"],
             'instrument': None
             }
         uuid.product = nc_header_augmenter.harmonize_model_file(data)
         return uuid
 
-    def fetch_volatile_model_uuid(self, model: str, raw_uuid: str) -> Union[str, None]:
-        payload = self._get_payload(model=model)
+    def fetch_volatile_model_uuid(self, row: dict) -> Union[str, None]:
+        payload = self._get_payload(model=row["model"]["id"])
         metadata = self.md_api.get('api/model-files', payload)
         try:
             uuid = self._read_stable_uuid(metadata)
@@ -81,7 +77,7 @@ class ProcessModel(ProcessBase):
                 uuid = self._read_volatile_uuid(metadata)
                 self._create_new_version = self._is_create_new_version(metadata)
         except MiscError as err:
-            self.update_statuses([raw_uuid], status='invalid')
+            self.update_statuses([row["uuid"]], status='invalid')
             msg = f'{err.message}: Setting status of {metadata[0]["filename"]} to "invalid"'
             raise MiscError(msg) from None
         return uuid
