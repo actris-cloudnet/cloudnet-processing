29984585760dceb25a615be3c706d11679d9063d
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-11-30 16:05:13 +0200

    Decode bit fields in HATPRO netCDF

--- src/housekeeping/__init__.py
+++ src/housekeeping/__init__.py
@@ -1,4 +1,4 @@
 from .exceptions import UnsupportedFile
-from .housekeeping import get_config, get_reader, list_instruments, write
+from .housekeeping import get_reader, list_instruments, write
 
-__all__ = ["get_config", "get_reader", "list_instruments", "write", "UnsupportedFile"]
+__all__ = ["get_reader", "list_instruments", "write", "UnsupportedFile"]
