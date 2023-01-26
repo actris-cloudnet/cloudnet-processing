9965f9f67aa35906e0ad0d4c10916d40b18b8bff
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-01-26 11:06:00 +0200

    Check response size and checksum from storage-service

--- src/data_processing/storage_api.py
+++ src/data_processing/storage_api.py
@@ -1,9 +1,15 @@
 """Metadata API for Cloudnet files."""
+import hashlib
 from os import path
+from typing import Literal
 
 import requests
 
 from data_processing import utils
+
+
+class StorageApiException(Exception):
+    pass
 
 
 class StorageApi:
@@ -27,8 +33,8 @@ class StorageApi:
         """Download raw files."""
         urls = [path.join(self._url, row["s3path"][1:]) for row in metadata]
         full_paths = [path.join(dir_name, row["filename"]) for row in metadata]
-        for args in zip(urls, full_paths):
-            self._get(*args)
+        for row, url, full_path in zip(metadata, urls, full_paths):
+            self._get(url, full_path, int(row["size"]), row["checksum"], "md5")
         uuids = [row["uuid"] for row in metadata]
         instrument_pids = [row["instrumentPid"] for row in metadata if "instrumentPid" in row]
         return full_paths, uuids, instrument_pids
@@ -40,7 +46,7 @@ class StorageApi:
         bucket = utils.get_product_bucket(metadata["volatile"])
         url = path.join(self._url, bucket, s3key)
         full_path = path.join(dir_name, filename)
-        self._get(url, full_path)
+        self._get(url, full_path, int(metadata["size"]), metadata["checksum"], "sha256")
         return full_path
 
     def delete_volatile_product(self, s3key: str) -> requests.Response:
@@ -60,12 +66,29 @@ class StorageApi:
         res.raise_for_status()
         return res
 
-    def _get(self, url: str, full_path: str) -> requests.Response:
-        res = self.session.get(url, auth=self._auth)
-        res.raise_for_status()
-        with open(full_path, "wb") as f:
-            f.write(res.content)
-        return res
+    def _get(
+        self,
+        url: str,
+        full_path: str,
+        size: int,
+        checksum: str,
+        checksum_algorithm: Literal["md5", "sha256"],
+    ):
+        res_size = 0
+        hash_sum = getattr(hashlib, checksum_algorithm)()
+        with open(full_path, "wb") as output:
+            with self.session.get(url, auth=self._auth, stream=True) as res:
+                res.raise_for_status()
+                for chunk in res.iter_content(chunk_size=8192):
+                    output.write(chunk)
+                    hash_sum.update(chunk)
+                    res_size += len(chunk)
+        if res_size != size:
+            raise StorageApiException(f"Invalid size: expected {size} bytes, got {res_size} bytes)")
+        if (res_checksum := hash_sum.hexdigest()) != checksum:
+            raise StorageApiException(
+                f"Invalid checksum: expected {checksum} bytes, got {res_checksum} bytes)"
+            )
 
     @staticmethod
     def _get_headers(full_path: str) -> dict:
