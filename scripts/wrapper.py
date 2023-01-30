013136d0b050359f06b7e0751e4856775427f210
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-01-30 10:47:07 +0200

    Capture output in wrapper.py

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+from subprocess import PIPE, STDOUT, Popen
 
 from data_processing import utils
 
 
 def main(args):
-    try:
-        subprocess.run(
-            args,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            encoding="utf-8",
-            errors="replace",
-        )
-    except subprocess.CalledProcessError as err:
-        utils.send_slack_alert(err, "wrapper", critical=True, log=err.output)
+    log = ""
+    with Popen(args, stdout=PIPE, stderr=STDOUT, encoding="utf-8", errors="replace") as proc:
+        while proc.poll() is None:
+            buf = proc.stdout.readline()
+            sys.stdout.write(buf)
+            log += buf
+        buf = proc.stdout.read()
+        sys.stdout.write(buf)
+        log += buf
+        if proc.returncode != 0:
+            utils.send_slack_alert(
+                Exception(f"Command returned non-zero exit status {proc.returncode}"),
+                "wrapper",
+                critical=True,
+                log=log,
+            )
 
 
 if __name__ == "__main__":
