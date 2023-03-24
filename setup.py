d5b097b6fc22bd2001efbdfc0394ca31f4877ab9
Simo Tukiainen <simo.tukiainen@fmi.fi>
2023-03-24 14:04:00 +0200

    Update `cloudnetpy` version requirement

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license="MIT License",
     install_requires=[
         "cloudnetpy_qc>=1.7.2",
-        "cloudnetpy[extras]>=1.46.3",
+        "cloudnetpy[extras]>=1.46.4",
         "rpgpy>=0.13.1",
         "pytest",
         "pylint",
