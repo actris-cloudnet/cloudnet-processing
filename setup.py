7a027af143ca3753a1f943cc247af6ae9ded21f8
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-04-21 08:57:28 +0300

    Update halo-reader

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "cloudnetpy_qc>=1.8.1",
         "cloudnetpy[extras]>=1.46.4",
         "rpgpy>=0.13.1",
-        "halo-reader==0.0.9",
+        "halo-reader>=0.1.0",
         "pytest",
         "pylint",
         "mypy",
