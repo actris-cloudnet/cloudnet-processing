a37adf5c4ede0f2ba4d50196ab8920a9b49bdae6
Simo Tukiainen <simo.tukiainen@fmi.fi>
2024-01-29 14:33:50 +0200

    Add support for Lindenberg ws

--- src/data_processing/nc_header_augmenter.py
+++ src/data_processing/nc_header_augmenter.py
@@ -187,6 +187,39 @@ def harmonize_parsivel_file(data: dict) -> str:
     return uuid
 
 
+def harmonize_ws_file(data: dict) -> str:
+    """Harmonizes weather station netCDF file."""
+    temp_file = NamedTemporaryFile()
+    with (
+        netCDF4.Dataset(data["full_path"], "r") as nc_raw,
+        netCDF4.Dataset(temp_file.name, "w", format="NETCDF4_CLASSIC") as nc,
+    ):
+        ws = Ws(nc_raw, nc, data)
+        valid_ind = ws.get_valid_time_indices()
+        ws.copy_ws_file_contents(time_ind=valid_ind)
+        ws.fix_variable_names()
+        ws.clean_global_attributes()
+        ws.add_global_attributes("weather-station", instruments.GENERIC_WEATHER_STATION)
+        ws.add_geolocation()
+        ws.add_date()
+        uuid = ws.add_uuid()
+        ws.clean_variable_attributes(
+            accepted_extra=("flag_values", "flag_meanings", "ancillary_variables")
+        )
+        for attribute in ("units", "long_name", "standard_name"):
+            ws.harmonize_attribute(attribute)
+        ws.add_history("weather-station")
+        ws.nc.source = "Weather station"
+        ws.fix_time()
+        ws.fix_rainfall_variables()
+        ws.fix_standard_names()
+        ws.fix_long_names()
+        ws.fix_flag_attributes()
+        ws.fix_ancillary_variable_names()
+        shutil.copy(temp_file.name, data["full_path"])
+    return uuid
+
+
 class Level1Nc:
     def __init__(self, nc_raw: netCDF4.Dataset, nc: netCDF4.Dataset, data: dict):
         self.nc_raw = nc_raw
@@ -307,9 +340,9 @@ class Level1Nc:
         for attribute in ("units", "long_name", "standard_name"):
             self.harmonize_attribute(attribute, (key,))
 
-    def clean_variable_attributes(self):
+    def clean_variable_attributes(self, accepted_extra: tuple | None = None):
         """Removes obsolete variable attributes."""
-        accepted = ("_FillValue", "units")
+        accepted = ("_FillValue", "units") + (accepted_extra or ())
         for _, item in self.nc.variables.items():
             for attr in item.ncattrs():
                 if attr not in accepted:
@@ -319,6 +352,19 @@ class Level1Nc:
         """Removes all global attributes."""
         for attr in self.nc.ncattrs():
             delattr(self.nc, attr)
+
+    def fix_name(self, keymap: dict):
+        for old_name, new_name in keymap.items():
+            if old_name in self.nc.variables:
+                self.nc.renameVariable(old_name, new_name)
+
+    def fix_attribute(self, keymap: dict, attribute: str):
+        """Fixes one attribute."""
+        if attribute not in ("units", "long_name", "standard_name"):
+            raise ValueError
+        for key, value in keymap.items():
+            if key in self.nc.variables:
+                self.nc.variables[key].__setattr__(attribute, value)
 
     def _copy_global_attributes(self):
         for name in self.nc_raw.ncattrs():
@@ -623,9 +669,7 @@ class ParsivelNc(Level1Nc):
             "velocity_center_classes": "velocity",
             "diameter_center_classes": "diameter",
         }
-        for old_name, new_name in keymap.items():
-            if old_name in self.nc.variables:
-                self.nc.renameVariable(old_name, new_name)
+        self.fix_name(keymap)
 
     def fix_global_attributes(self):
         for attr in ("Sensor_ID", "sensor_serial_number"):
@@ -754,10 +798,7 @@ class ParsivelNc(Level1Nc):
             "fall_velocity": "Average velocity of each diameter class",
             "diameter": "Center diameter of precipitation particles",
         }
-        for key, long_name in keymap.items():
-            if key in self.nc.variables:
-                self.nc.variables[key].long_name = long_name
-
+        self.fix_attribute(keymap, "long_name")
         skip = ("time", "visibility", "synop_WaWa", "synop_WW")
         for key in self.nc.variables:
             if key not in skip:
@@ -787,9 +828,7 @@ class ParsivelNc(Level1Nc):
             "diameter": "m",
             "diameter_spread": "m",
         }
-        for key, units in keymap.items():
-            if key in self.nc.variables:
-                self.nc.variables[key].units = units
+        self.fix_attribute(keymap, "units")
 
     def fix_standard_names(self):
         for key in self.nc.variables:
@@ -807,9 +846,7 @@ class ParsivelNc(Level1Nc):
             "radar_reflectivity": "equivalent_reflectivity_factor",
             "rainfall_rate": "rainfall_rate",
         }
-        for key, standard_name in keymap.items():
-            if key in self.nc.variables:
-                self.nc.variables[key].standard_name = standard_name
+        self.fix_attribute(keymap, "standard_name")
 
     def fix_comments(self):
         for key, item in ATTRIBUTES.items():
@@ -820,6 +857,123 @@ class ParsivelNc(Level1Nc):
             else:
                 if hasattr(self.nc.variables[key], "comment"):
                     delattr(self.nc.variables[key], "comment")
+
+
+class Ws(Level1Nc):
+    def copy_ws_file_contents(self, time_ind: list):
+        self.nc.createDimension("time", len(time_ind))
+        for name, var_in in self.nc_raw.variables.items():
+            dim = var_in.dimensions
+            dtype = var_in.dtype
+            if name == "time":
+                dtype = "f8"
+            elif len(var_in[:]) == 1:
+                dim = ()
+            var = self.nc.createVariable(
+                name,
+                dtype,
+                dim,
+                zlib=True,
+                fill_value=getattr(var_in, "_FillValue", None),
+            )
+            self._copy_variable_attributes(var_in, var)
+            var[:] = var_in[time_ind] if "time" in var_in.dimensions else var_in[:]
+
+    def get_valid_time_indices(self) -> list:
+        """Finds valid time indices."""
+        time_stamps = self.nc_raw.variables["time"][:]
+        valid_ind: list[int] = []
+        minutes_in_day = 1440
+        for ind, t in enumerate(time_stamps):
+            if 0 < t < minutes_in_day:
+                if len(valid_ind) > 1 and t <= time_stamps[valid_ind[-1]]:
+                    continue
+                valid_ind.append(ind)
+        if not valid_ind:
+            raise ValidTimeStampError
+        return valid_ind
+
+    def fix_time(self):
+        """Fixes time units."""
+        time = self.nc.variables["time"][:]
+        ind = time > 0
+        self.nc.variables["time"][ind] = time[ind] / 60
+        self.nc.variables["time"].units = self._get_time_units()
+        self.nc.variables["time"].calendar = "standard"
+
+    def fix_variable_names(self):
+        keymap = {
+            "lat": "latitude",
+            "lon": "longitude",
+            "pa": "air_pressure",
+            "ta": "air_temperature",
+            "td": "dew_point_temperature",
+            "vis": "visibility",
+            "precip": "precipitation",
+            "wspeed": "wind_speed",
+            "wdir": "wind_direction",
+            "wdir_flag": "wind_direction_quality_flag",
+            "wspeed_gust": "wind_speed_gust",
+            "wspeed_gust_flag": "wind_speed_gust_quality_flag",
+            "pa_flag": "air_pressure_quality_flag",
+            "ta_flag": "air_temperature_quality_flag",
+            "td_flag": "dew_point_temperature_quality_flag",
+            "hur": "relative_humidity",
+            "hur_flag": "relative_humidity_quality_flag",
+            "vis_flag": "visibility_quality_flag",
+            "wspeed_flag": "wind_speed_quality_flag",
+            "precipitation": "rainfall_rate",
+            "precip_flag": "rainfall_rate_quality_flag",
+        }
+        self.fix_name(keymap)
+
+    def fix_long_names(self):
+        keymap = {
+            "air_pressure_quality_flag": "Air pressure quality flag",
+            "air_temperature_quality_flag": "Air temperature quality flag",
+            "dew_point_temperature_quality_flag": "Dew point temperature quality flag",
+            "relative_humidity_quality_flag": "Relative humidity quality flag",
+            "wind_direction_quality_flag": "Wind direction quality flag",
+            "wind_speed_gust_quality_flag": "Wind speed gust quality flag",
+            "wind_speed_gust": "Wind speed gust",
+            "visibility_quality_flag": "Visibility quality flag",
+            "dew_point_temperature": "Dew point temperature",
+            "wind_speed_quality_flag": "Wind speed quality flag",
+            "rainfall_rate_quality_flag": "Rainfall rate quality flag",
+            "visibility": "Meteorological optical range (MOR) visibility",
+            "rainfall_amount": "Rainfall amount",
+        }
+        self.fix_attribute(keymap, "long_name")
+
+    def fix_standard_names(self):
+        keymap = {
+            "visibility": "visibility_in_air",
+            "rainfall_amount": "thickness_of_rainfall_amount",
+            "dew_point_temperature": "dew_point_temperature",
+            "wind_speed_gust": "wind_speed_of_gust",
+        }
+        self.fix_attribute(keymap, "standard_name")
+
+    def fix_ancillary_variable_names(self):
+        for key, var in self.nc.variables.items():
+            name = f"{key}_quality_flag"
+            if hasattr(var, "ancillary_variables") and name in self.nc.variables:
+                var.ancillary_variables = name
+
+    def fix_rainfall_variables(self):
+        orig_data = self.nc["rainfall_rate"][:]
+        dt = np.median(np.diff(self.nc.variables["time"][:]))
+        rainfall_rate = to_ms1("rainfall_rate", orig_data / dt, "mm h-1")
+        self.nc["rainfall_rate"][:] = rainfall_rate
+        self.nc.createVariable("rainfall_amount", "f4", ("time",), zlib=True)
+        self.nc["rainfall_amount"][:] = np.cumsum(orig_data) / 1e3  # mm -> m
+        self.nc["rainfall_amount"].units = "m"
+
+    def fix_flag_attributes(self):
+        for key, var in self.nc.variables.items():
+            if key.endswith("_flag") and hasattr(var, "flag_values"):
+                var.flag_values = np.array(var.flag_values, dtype="i1")
+                var.units = "1"
 
 
 def to_ms1(variable: str, data: np.ndarray, units: str) -> np.ndarray:
