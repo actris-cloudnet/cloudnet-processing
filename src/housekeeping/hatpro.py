cf2247a8305a65091e2db86ec03df8f3d749fa29
Simo Tukiainen <simo.tukiainen@fmi.fi>
2024-10-23 10:20:28 +0300

    Fix type hints

--- src/housekeeping/hatpro.py
+++ src/housekeeping/hatpro.py
@@ -103,6 +103,8 @@ class HatproHkd:
 
 class HatproHkdNc:
     def __init__(self, filename: str | bytes | PathLike):
+        if isinstance(filename, bytes):
+            filename = filename.decode()
         with netCDF4.Dataset(filename) as nc:
             time_ref = nc.variables.get("time_reference")
             if time_ref and time_ref[0] != TIME_REF_UTC:
