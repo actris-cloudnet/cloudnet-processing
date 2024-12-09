831b64fc118419121e2cd648f6f9cd338719d9b6
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-12-09 11:59:35 +0200

    Support old CL61 housekeeping format from attributes

--- src/housekeeping/cl61.py
+++ src/housekeeping/cl61.py
@@ -1,4 +1,5 @@
 import netCDF4
+import numpy as np
 
 from .utils import cftime2datetime64
 
@@ -8,12 +9,22 @@ HPA_TO_PA = 100
 
 def read_cl61(nc: netCDF4.Dataset) -> dict:
     grp = nc.groups["monitoring"]
-    measurements = {var: grp[var][:] for var in grp.variables.keys()}
-    measurements["time"] = cftime2datetime64(grp["time"])
+    if hasattr(grp, "Timestamp"):
+        measurements = {
+            key: np.array([getattr(grp, key)])
+            for key in grp.ncattrs()
+            if key != "Timestamp"
+        }
+        dt = round(float(grp.Timestamp))
+        measurements["time"] = np.array([dt], dtype="datetime64[s]")
+    else:
+        measurements = {key: value[:] for key, value in grp.variables.items()}
+        measurements["time"] = cftime2datetime64(grp["time"])
     measurements["internal_temperature"] = measurements["internal_temperature"] + C_TO_K
     measurements["internal_pressure"] = measurements["internal_pressure"] * HPA_TO_PA
     measurements["laser_temperature"] = measurements["laser_temperature"] + C_TO_K
-    measurements["transmitter_enclosure_temperature"] = (
-        measurements["transmitter_enclosure_temperature"] + C_TO_K
-    )
+    if "transmitter_enclosure_temperature" in measurements:
+        measurements["transmitter_enclosure_temperature"] = (
+            measurements["transmitter_enclosure_temperature"] + C_TO_K
+        )
     return measurements
