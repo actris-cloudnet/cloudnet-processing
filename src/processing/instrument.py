fa6ba047c818d0e9273a90ac1dbf0e94bd226e53
Niko Leskinen <niko.leskinen@fmi.fi>
2024-12-10 12:00:32 +0200

    Halo housekeeping data processing

--- src/processing/instrument.py
+++ src/processing/instrument.py
@@ -118,13 +118,44 @@ def _process_housekeeping(processor: Processor, params: InstrumentParams) -> Non
         return
     logging.info("Processing housekeeping data")
     raw_api = utils.RawApi(processor.md_api.config, processor.md_api.session)
-    payload = processor._get_payload(
-        site=params.site.id, date=params.date, instrument_pid=params.instrument.pid
-    )
-    records = processor.md_api.get("api/raw-files", payload)
+    records = _get_housekeeping_records(processor, params)
     try:
         with housekeeping.Database() as db:
             for record in records:
                 housekeeping.process_record(record, raw_api=raw_api, db=db)
     except housekeeping.HousekeepingException:
         logging.exception("Housekeeping failed")
+
+
+def _get_housekeeping_records(
+    processor: Processor, params: InstrumentParams
+) -> list[dict]:
+    if params.instrument.type == "halo-doppler-lidar":
+        first_day_of_month = params.date.replace(day=1)
+        payload = processor._get_payload(
+            site=params.site.id,
+            date=(first_day_of_month, params.date),
+            instrument_pid=params.instrument.pid,
+            filename_prefix="system_parameters",
+        )
+        records = processor.md_api.get("api/raw-files", payload)
+        return _select_halo_doppler_lidar_hkd_records(records)
+
+    payload = processor._get_payload(
+        site=params.site.id, date=params.date, instrument_pid=params.instrument.pid
+    )
+    return processor.md_api.get("api/raw-files", payload)
+
+
+def _select_halo_doppler_lidar_hkd_records(records: list[dict]) -> list[dict]:
+    return [
+        max(
+            records,
+            key=lambda x: (
+                x.get("measurementDate"),
+                x.get("createdAt"),
+                x.get("updatedAt"),
+                x.get("size"),
+            ),
+        )
+    ]
