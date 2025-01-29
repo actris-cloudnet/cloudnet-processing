c4cc8f8d1e021d2ad6cf19a9dcd8ca6a1317e385
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-01-29 11:17:11 +0200

    Support Jülich weather station

--- src/processing/instrument_process.py
+++ src/processing/instrument_process.py
@@ -661,6 +661,7 @@ class ProcessWeatherStation(ProcessInstrument):
             "bucharest",
             "galati",
             "munich",
+            "juelich",
         )
         if self.params.site.id not in supported_sites:
             raise NotImplementedError("Weather station not implemented for this site")
