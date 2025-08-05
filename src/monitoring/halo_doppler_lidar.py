a82b3ff3f9fb15e575667b36077d54bae4d82fc2
Niko Leskinen <niko.leskinen@fmi.fi>
2025-08-05 17:13:16 +0300

    Nicer plots

--- src/monitoring/halo_doppler_lidar.py
+++ src/monitoring/halo_doppler_lidar.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import doppy
 import matplotlib.pyplot as plt
+import matplotlib.dates
 import numpy as np
 from processing.processor import Processor
 
@@ -35,6 +36,8 @@ def process_system_parameters(
     measurement_date_offset = datetime.timedelta(days=31)
     date_start_off = date_start - measurement_date_offset
     date_end_off = date_end + measurement_date_offset
+
+    print(instrument)
 
     with tempfile.TemporaryDirectory() as tempdir:
         paths, _ = processor.download_instrument(
@@ -78,13 +81,72 @@ def process_system_parameters(
             )
 
 
+def _format_time_axis(ax):
+    locator = matplotlib.dates.AutoDateLocator()
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(matplotlib.dates.ConciseDateFormatter(locator))
+
+
+def _pretty_ax(ax):
+    ax.set_facecolor("#f0f0f0")
+    ax.grid(True, axis="y", color="white", linestyle="-", linewidth=4)
+    ax.set_axisbelow(True)
+    ax.tick_params(
+        axis="both",
+        length=4,
+        width=2,
+        direction="out",
+        pad=10,
+    )
+
+    ax.spines["left"].set_position(("outward", 10))
+    ax.spines["bottom"].set_position(("outward", 10))
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+
+
+def _pretty_fig(fig):
+    width_px = 800
+    height_px = 600
+    dpi = 100
+    fig.set_size_inches(width_px / dpi, height_px / dpi)
+
+
+def _save_fig(fig, path: Path) -> tuple[int, int]:
+    aspect = 16 / 9
+    dpi = 400
+    height_inch = 16
+    height_px = height_inch * dpi
+    width_px = aspect * height_px
+    fig.set_size_inches(width_px / dpi, height_px / dpi)
+
+    font_size = 22
+    for ax in fig.axes:
+        ax.tick_params(axis="both", labelsize=font_size)
+        ax.xaxis.label.set_size(font_size)
+        ax.yaxis.label.set_size(font_size)
+        ax.title.set_size(font_size + 2)
+        ax.xaxis.get_offset_text().set_fontsize(font_size)
+        ax.yaxis.get_offset_text().set_fontsize(font_size)
+
+    fig.savefig(path, dpi=dpi, bbox_inches="tight", pad_inches=0)
+    return int(width_px), int(height_px)
+
+
 def _plot_sys_params_var(
     var: str, sys_params: doppy.raw.HaloSysParams, img_path: Path, variable_id: str
 ) -> MonitoringVisualization:
     fig, ax = plt.subplots()
     y = getattr(sys_params, var)
     ax.scatter(sys_params.time, y)
+    _format_time_axis(ax)
+    _pretty_ax(ax)
+    _pretty_fig(fig)
+    #width, height = _save_fig(fig, "/develop/figs/img.png")
+    width, height = _save_fig(fig, img_path)
     fig.savefig(img_path)
     return MonitoringVisualization(
-        img_path, variable_id, Dimensions(100, 100, 0, 0, 0, 0)
+        img_path, variable_id, Dimensions(width, height, 0, 0, 0, 0)
     )
