40862b8c308cbbb9a69d54ccf5bde777b281dfb8
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-08-14 15:40:57 +0300

    Remove unused methods

--- src/processing/metadata_api.py
+++ src/processing/metadata_api.py
@@ -1,6 +1,5 @@
 """Metadata API for Cloudnet files."""
 
-import logging
 import uuid
 
 import requests
@@ -45,15 +44,6 @@ class MetadataApi:
         res.raise_for_status()
         return res
 
-    def put_file(
-        self, end_point: str, resource: str, full_path: str, auth: tuple[str, str]
-    ) -> requests.Response:
-        """PUT file to Cloudnet data portal."""
-        url = f"{self._url}/{end_point}/{resource}"
-        res = self.session.put(url, data=open(full_path, "rb"), auth=auth)
-        res.raise_for_status()
-        return res
-
     def delete(self, end_point: str, params: dict | None = None) -> requests.Response:
         """Delete Cloudnet metadata."""
         url = f"{self._url}/{end_point}"
@@ -69,25 +59,6 @@ class MetadataApi:
                 "dimensions": data["dimensions"],
             }
             self.put("visualizations", data["s3key"], payload)
-
-    def upload_instrument_file(
-        self, base, instrument: str, filename: str, instrument_pid: str | None = None
-    ):
-        checksum = utils.md5sum(base.daily_file.name)
-        metadata = {
-            "filename": filename,
-            "checksum": checksum,
-            "instrument": instrument,
-            "measurementDate": base.date_str,
-            "site": base.site,
-            "instrumentPid": instrument_pid,
-        }
-        try:
-            self.post("upload/metadata", metadata, auth=self._auth)
-        except requests.exceptions.HTTPError as err:
-            logging.info(err)
-            return
-        self.put_file("upload/data", checksum, base.daily_file.name, self._auth)
 
     def update_dvas_info(self, uuid: uuid.UUID, timestamp: str, dvas_id: str):
         payload = {"uuid": uuid, "dvasUpdatedAt": timestamp, "dvasId": dvas_id}
